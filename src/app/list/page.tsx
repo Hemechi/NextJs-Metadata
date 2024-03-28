0b2259c7834acfb430ce6abed71278981e89d011
Hemechi <helen.leang15@gmail.com>
2024-03-28 09:01:43 +0700

    done

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,13 +1,6 @@
 'use client'
  
-import { Metadata } from 'next';
 import { useSearchParams } from 'next/navigation'
-
-export const metadata: Metadata = {
-  title: "List",
-  description: "This is List page shop",
-  keywords: ['shop', 'ecommerce', 'sell',"card"]
-};
  
 export default function SortProducts() {
   const searchParams = useSearchParams()
